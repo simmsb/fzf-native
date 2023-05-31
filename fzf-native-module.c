6bd732a02c822c2d60c17ebd0e0439ab7631dc89
Ben Simms <ben@bensimms.moe>
2023-05-31 18:03:21 +0100

    Work on latest emacs (?)

--- fzf-native-module.c
+++ fzf-native-module.c
@@ -5,10 +5,18 @@
 #include "emacs-module.h"
 #include "fzf.h"
 
-#ifdef _WIN32
-#  define EXPORT __declspec(dllexport)
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef __GNUC__
+    #define EXPORT __attribute__ ((dllexport))
+  #else
+    #define EXPORT __declspec(dllexport)
+  #endif
 #else
-#  define EXPORT
+  #if __GNUC__ >= 4
+    #define EXPORT __attribute__ ((visibility ("default")))
+  #else
+    #define EXPORT
+  #endif
 #endif
 
 /** See https://wambold.com/Martin/writings/alignof.html */
@@ -203,7 +211,7 @@ emacs_value fzf_native_make_slab(emacs_env UNUSED(*env),
   return env->make_user_ptr(env, slab_finalize, slab);
 }
 
-int emacs_module_init(struct emacs_runtime *rt) {
+EXPORT int emacs_module_init(struct emacs_runtime *rt) {
   // Verify compatability with Emacs executable loading this module
   if ((size_t) rt->size < sizeof *rt)
     return 1;
@@ -212,6 +220,7 @@ int emacs_module_init(struct emacs_runtime *rt) {
     return 2;
 
   static struct Data data;
+
 
   // fzf-native-score STR QUERY &optional SLAB
   env->funcall(env, env->intern(env, "defalias"), 2, (emacs_value[]) {
